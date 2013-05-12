35d3298e5b87c596d2615700058f8f811263cd70
ngacarine <paulecarinel@yahoo.fr>
2013-05-12 19:01:43 +0200

    Update ExigenceController.php

--- application/controllers/ExigenceController.php
+++ application/controllers/ExigenceController.php
@@ -2,7 +2,7 @@
 /**
  * ExigenceController
  * 
- * @author
+ * @author carine
  * @version 
  */
 require_once 'Zend/Controller/Action.php';
@@ -19,12 +19,12 @@ class ExigenceController extends Zend_Controller_Action
     {
         $AFI = new Application_Form_Exigence();
         $this->view->formulaireInscri = $AFI;
-        //nous vérifions que les données provenantes du formulaire sont envoyées par la méthode POST
+        //nous vï¿½rifions que les donnï¿½es provenantes du formulaire sont envoyï¿½es par la mï¿½thode POST
         if ($this->getRequest()->isPost()) {
             $formData = $this->getRequest()->getPost();
-            /*chargement des données dans la variable $AFI
+            /*chargement des donnï¿½es dans la variable $AFI
              $AFI->populate($formData);
-             Si le formulaire est vailde, on récupère les données saisies par l'utilisateur à l'aide 
+             Si le formulaire est vailde, on rï¿½cupï¿½re les donnï¿½es saisies par l'utilisateur ï¿½ l'aide 
              de getValue */
             if ($AFI->isValid($formData)) {
                 $nom = $AFI->getValue('nom');
@@ -36,9 +36,9 @@ class ExigenceController extends Zend_Controller_Action
                 $id_projet=$AFI->getValue('id_projet');
                 $user = new Application_Model_DbTable_Exigences();
                 $user->addUser($nom, $prenom, $adresse, $login, $password,$priorite,$id_projet);
-                //la redirection de l'utilisateur après inscription
+                //la redirection de l'utilisateur aprï¿½s inscription
                 $this->_helper->redirector('lst');
-                 // donner la possibilité à l'utilisateur de réinsérer les données
+                 // donner la possibilitï¿½ ï¿½ l'utilisateur de rï¿½insï¿½rer les donnï¿½es
             } else {
                 $AFI->populate($formData);
             }
@@ -48,11 +48,11 @@ public function mainAction ()
     {
         $B = new Zend_View_Helper_BaseUrl();
         $this->view->ma=$B;
-         $this->view->contenu=" En tant qu'administrateur vous pouvez gèrer les projets,les collaborateur 
+         $this->view->contenu=" En tant qu'administrateur vous pouvez gï¿½rer les projets,les collaborateur 
          ainsi que les managers,ce site vous permet de bien gerer l'avancement de vos projets.";
     }
     /*Cette fonction "authAction" permet d'instancier la classe 
-      "Application_Form_Authentification" que nous avons créé préalablement 
+      "Application_Form_Authentification" que nous avons crï¿½ï¿½ prï¿½alablement 
       et la charger dans une variable ("formulaireAuth") dans la vue de l'objet courant */
     public function corpAction(){
     $this->view->contenu=" Cum sociis natoque penatibus et magnis dis parturient montes,
@@ -66,9 +66,9 @@ public function mainAction ()
         $this->view->formulaireAuth = $AFA;
          if ($this->getRequest()->isPost()) {
             $formData = $this->getRequest()->getPost();
-            /*chargement des données dans la variable $AFI
+            /*chargement des donnï¿½es dans la variable $AFI
              $AFI->populate($formData);
-             Si le formulaire est vailde, on récupère les données saisies par l'utilisateur à l'aide 
+             Si le formulaire est vailde, on rï¿½cupï¿½re les donnï¿½es saisies par l'utilisateur ï¿½ l'aide 
              de getValue */
             if ($AFA->isValid($formData)) {
             	if($AFA->getValue('login')=='admin' && $AFA->getValue('password')=='admin'){
@@ -88,9 +88,9 @@ public function mainAction ()
     //Cette fonction permet de lister le contenu de la Table "utilisateur"
     public function lstAction ()
     {
-        //instanciation du modèle "utilisateur" pour extraire les données de la base de données 
+        //instanciation du modï¿½le "utilisateur" pour extraire les donnï¿½es de la base de donnï¿½es 
         $ut = new Application_Model_DbTable_Exigences();
-        /* la fonction "fetchAll" permet de parcourir la base de données et inclure le résultat dans l'objet 
+        /* la fonction "fetchAll" permet de parcourir la base de donnï¿½es et inclure le rï¿½sultat dans l'objet 
          "users" qui sera parcouru dans la vue "lst" . (voir la vue lst.phtml) */
         $this->view->users = $ut->fetchAll();
     }
@@ -103,14 +103,14 @@ public function mainAction ()
         $form = new Application_Form_Exigence();
         //On change le label du bouton "valider" par "Save"
         $form->ajouter->setLabel('Save');
-        //on affecte ce nouveau formulaire à la vue "edit" qui contient affiche l'objet "form2" (voir la vue edit.phtml)
+        //on affecte ce nouveau formulaire ï¿½ la vue "edit" qui contient affiche l'objet "form2" (voir la vue edit.phtml)
         $this->view->form2 = $form;
-        /* on teste si le contenu du formulaire est prêt à l'envoi
-        puis on prépare la récupèration de son contenu */
+        /* on teste si le contenu du formulaire est prï¿½t ï¿½ l'envoi
+        puis on prï¿½pare la rï¿½cupï¿½ration de son contenu */
         if ($this->getRequest()->isPost()) {
             $formData = $this->getRequest()->getPost();
-            /* si les données sont valides on récupère chaque valeur de chaque 
-            champs pour les éditer*/
+            /* si les donnï¿½es sont valides on rï¿½cupï¿½re chaque valeur de chaque 
+            champs pour les ï¿½diter*/
             if ($form->isValid($formData)) {
                 $id = (int) $form->getValue('id');
                 $nom = $form->getValue('nom');
@@ -124,24 +124,24 @@ public function mainAction ()
                 //on instancie un nouvel objet Application_Model_DbTable_Utilisateur
                 $users = new Application_Model_DbTable_Exigences();
                 
-                //on fait appel à la fonction "updateUser" de la classe "Application_Model_DbTable_Utilisateur"
+                //on fait appel ï¿½ la fonction "updateUser" de la classe "Application_Model_DbTable_Utilisateur"
                 $users->updateUser($id, $nom, $prenom, $adresse, $login, $password,$priorite,$id_projet);
                 
-                //après la mise à jour on redirige l'utilisateur vers l'action "lst"
+                //aprï¿½s la mise ï¿½ jour on redirige l'utilisateur vers l'action "lst"
                 $this->_helper->redirector('lst');
                 
             } 
             
             else 
             {
-            	/*Si les données ne sont pas valides, on rempli de nouveau le formulaire avec 
-            	les valeurs par défaut provenant de la Base de données (c'est le rôle de la fonction populate) */
+            	/*Si les donnï¿½es ne sont pas valides, on rempli de nouveau le formulaire avec 
+            	les valeurs par dï¿½faut provenant de la Base de donnï¿½es (c'est le rï¿½le de la fonction populate) */
                 $form->populate($formData);
             }
         } else {
-        	/*Dans le cas où le formulaire n'envoie pas les données, c-a-d "$this->getRequest->isPost" n'est pas vérifiée
-        	on récupère l'id pour reboucler sur le même contenu de la ligne à modifier.
-        	La fonction _getParam permet de récupérer la valeur de l'id sinon elle prend par défaut la valeur '0' */
+        	/*Dans le cas oï¿½ le formulaire n'envoie pas les donnï¿½es, c-a-d "$this->getRequest->isPost" n'est pas vï¿½rifiï¿½e
+        	on rï¿½cupï¿½re l'id pour reboucler sur le mï¿½me contenu de la ligne ï¿½ modifier.
+        	La fonction _getParam permet de rï¿½cupï¿½rer la valeur de l'id sinon elle prend par dï¿½faut la valeur '0' */
             $id = $this->_getParam('id', 0);
             if ($id > 0) {
                 $users = new Application_Model_DbTable_Exigences();
@@ -150,22 +150,22 @@ public function mainAction ()
         }
     }
     
-    //la fonction deleteAction permet de supprimer un enregistrement de la base de données
+    //la fonction deleteAction permet de supprimer un enregistrement de la base de donnï¿½es
     public function deleteAction ()
     {
         if ($this->getRequest()->isPost()) {
         	
-        	/* on récupère dans la variable $del la valeur du champs "del" de notre formulaire qui peut être
+        	/* on rï¿½cupï¿½re dans la variable $del la valeur du champs "del" de notre formulaire qui peut ï¿½tre
         	 soit "YES" soit "NO"*/
             $del = $this->getRequest()->getPost('del');
-            //Si la valeur de "del" est "YES" c'est que l'utilisateur a confirmé la suppression
+            //Si la valeur de "del" est "YES" c'est que l'utilisateur a confirmï¿½ la suppression
             if ($del == 'YES') {
                 //$id = $this->_getParam('id', 0);
                 $id = $this->getRequest()->getPost('id');
                 $users = new Application_Model_DbTable_Exigences();
                 $users->deleteUser($id);
             }
-            //puis on redirige de nouveau vers l'action "lst" pour revenir à la liste des utilisateurs
+            //puis on redirige de nouveau vers l'action "lst" pour revenir ï¿½ la liste des utilisateurs
             $this->_helper->redirector('lst');
         } else {
             $id = $this->_getParam('id', 0);
